c2aaae0f0e3026426ea5407b41fedee37c4b2ee4
MARYY0412 <mateusz.rybaczyk@spoko.pl>
2022-11-13 17:17:43 +0100

    stylizowanie menu, dodawanie ikon do menu, dodanie obrazka jako tła, przeźroczyste elementy

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import styled from 'styled-components'
+
+
 
 function Home() {
   return (
-    <div>Home</div>
+    <Container>Home</Container>
   )
 }
 
 export default Home
+
+const Container = styled.div`
+    background-color: rgba(255,255,255, 0.8);
+    backdrop-filter: blur(2px);
+
+    width: 90%;
+    height: 40rem;
+`
