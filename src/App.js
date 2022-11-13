66445ee7605b4be18bdd45089e3cd130805987bc
MARYY0412 <mateusz.rybaczyk@spoko.pl>
2022-11-13 13:12:43 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -3,12 +3,6 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
-      <h1>hello</h1>
-      <h1>hello</h1>
-      <h1>hello</h1>
-
-
     </div>
   );
 }
